3a524ec3cf04609084772de46c99b44f366e4b32
zmelich <stocek86@gmail.com>
2026-05-15 10:16:13 +0200

    Getting the list of beers as Java Map.

--- src/main/java/guru/springframework/spring7resttemplate/client/BeerClientImpl.java
+++ src/main/java/guru/springframework/spring7resttemplate/client/BeerClientImpl.java
@@ -13,11 +13,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Map;
+
 @Service
 @RequiredArgsConstructor
 public class BeerClientImpl implements BeerClient {
 
     private final RestTemplateBuilder restTemplateBuilder;
+
+    private static final String BASE_URL = "http://localhost:8080";
+    private static final String GET_BEER_PATH="/api/v1/beer";
 
     @Override
     public Page<BeerDTO> listBeers() {
@@ -25,7 +30,11 @@ public class BeerClientImpl implements BeerClient {
         RestTemplate  restTemplate = restTemplateBuilder.build();
 
         ResponseEntity<String> stringResponseEntity = restTemplate
-                .getForEntity("http://localhost:8080/api/v1/beer", String.class);
+                .getForEntity(BASE_URL+GET_BEER_PATH, String.class);
+
+        ResponseEntity<Map> mapResponseEntity = restTemplate
+                .getForEntity(BASE_URL+GET_BEER_PATH, Map.class);
+
 
         System.out.println(stringResponseEntity.getBody());
 
