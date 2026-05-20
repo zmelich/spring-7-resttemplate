46b96b4f18f279be31908b9d092dd0a47f063547
zmelich <stocek86@gmail.com>
2026-05-20 13:46:22 +0200

    Start using query parameters via UriComponentsBuilder with beerName.

--- src/test/java/guru/springframework/spring7resttemplate/client/BeerClientImplTest.java
+++ src/test/java/guru/springframework/spring7resttemplate/client/BeerClientImplTest.java
@@ -13,9 +13,15 @@ class BeerClientImplTest {
     BeerClientImpl beerClient;
 
     @Test
+    void listBeersNoBeerName() {
+
+        beerClient.listBeers(null);
+    }
+
+    @Test
     void listBeers() {
 
-        beerClient.listBeers();
+        beerClient.listBeers("ALE");
 
     }
 }
