d044fbadca0fcd90f462b669fcab06dd7f6e841c
zmelich <stocek86@gmail.com>
2026-06-18 17:20:40 +0200

    Add testListBeersWithQueryParam() method to BeerClientMockTest class.

--- src/test/java/guru/springframework/spring7resttemplate/client/BeerClientMockTest.java
+++ src/test/java/guru/springframework/spring7resttemplate/client/BeerClientMockTest.java
@@ -77,6 +77,27 @@ public class BeerClientMockTest {
     }
 
     @Test
+    void testListBeersWithQueryParam() {
+        String response = objectMapper.writeValueAsString(getPage());
+
+        URI uri = UriComponentsBuilder.fromUriString(URL + BeerClientImpl.GET_BEER_PATH)
+                .queryParam("beerName", "ALE")
+                .build()
+                .toUri();
+
+        server.expect(method(HttpMethod.GET))
+                .andExpect(requestTo(uri))
+                .andExpect(queryParam("beerName", "ALE"))
+                .andRespond(withSuccess(response, MediaType.APPLICATION_JSON));
+
+        Page<BeerDTO> responsePage = beerClient.
+                listBeers("ALE", null, null, null, null);
+
+        assertThat(responsePage.getContent()).size().isEqualTo(1);
+
+    }
+
+    @Test
     void testDeleteNotFound() {
         server.expect(method(HttpMethod.DELETE))
                 .andExpect(requestToUriTemplate(URL + BeerClientImpl.GET_BEER_BY_ID_PATH, dto.getId()))
